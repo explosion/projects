b420ef3a2787563c0f4b0e797c49047501aa6b00
Ines Montani <ines@ines.io>
2020-09-22 12:17:12 +0200

    Update test

--- tutorials/nel_emerson/test_project_nel_emerson.py
+++ tutorials/nel_emerson/test_project_nel_emerson.py
@@ -1,8 +1,10 @@
 from spacy.cli.project.run import project_run
 from spacy.cli.project.assets import project_assets
 from pathlib import Path
+import pytest
 
 
+@pytest.mark.skip(reason="Not ready yet")
 def test_project():
     root = Path(__file__).parent
     project_run(root, "setup")
