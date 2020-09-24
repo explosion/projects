d6c25cb1aa6b13cde013a61bf93976f15244d3bb
Ines Montani <ines@ines.io>
2020-09-24 20:49:54 +0200

    Update models

--- integrations/fastapi/scripts/download_models.sh
+++ integrations/fastapi/scripts/download_models.sh
@@ -1,5 +1,4 @@
 python -m spacy download en_core_web_sm
 python -m spacy download en_core_web_md
 python -m spacy download en_core_web_lg
-# TODO: add when model is uploaded
-# python -m spacy download en_core_web_trf
+python -m spacy download en_core_web_trf
