128b4eabadf8d3b7ded11f5f129d47f97e33c73b
svlandeg <sofie.vanlandeghem@gmail.com>
2020-09-28 22:16:32 +0200

    logger fix

--- benchmarks/speed/scripts/logger.py
+++ benchmarks/speed/scripts/logger.py
@@ -7,7 +7,7 @@ def create_logger(results_dir: Path) -> Callable:
     write_header = not results_file.exists()
     with results_file.open("a", encoding="utf8") as f:
         if write_header:
-            f.write("library,name;gpu;articles;characters;words;seconds;k wps")
+            f.write("library;name;gpu;articles;characters;words;seconds;k wps")
             f.write("\n")
 
     def log_result(
@@ -16,7 +16,7 @@ def create_logger(results_dir: Path) -> Callable:
         wps = words / seconds
         wps = wps / 1000
         with results_file.open("a", encoding="utf8") as f:
-            f.write(f"{library},{name};{gpu};{articles};{characters};{words};{seconds};{wps}")
+            f.write(f"{library};{name};{gpu};{articles};{characters};{words};{seconds};{wps}")
             f.write("\n")
 
     return log_result
