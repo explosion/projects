e8d2dc2e2a07dc1aa123dce8dc1ea621adedac7c
svlandeg <sofie.vanlandeghem@gmail.com>
2020-10-07 19:38:42 +0200

    update speed project so the test runs for now

--- benchmarks/speed/test_project_speed_benchmark.py
+++ benchmarks/speed/test_project_speed_benchmark.py
@@ -1,10 +1,9 @@
 from spacy.cli.project.run import project_run
-from spacy.cli.project.assets import project_assets
 from pathlib import Path
 
 
 def test_project():
     root = Path(__file__).parent
-    project_assets(root)
+    # project_assets(root)   # there are currently no assets defined for this project
     project_run(root, "setup")
     project_run(root, "benchmark")
