28c02f8c7c3f5873bcd857b8045101c67ca3c320
Ines Montani <ines@ines.io>
2020-10-11 13:08:06 +0200

    Fix FastAPI requirements

--- integrations/fastapi/scripts/download_models.sh
+++ integrations/fastapi/scripts/download_models.sh
@@ -2,4 +2,5 @@
 python -m spacy download en_core_web_sm
 python -m spacy download en_core_web_md
 python -m spacy download en_core_web_lg
-python -m spacy download en_core_web_trf
+# TODO: update when package is available
+# python -m spacy download en_core_web_trf
