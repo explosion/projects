a50eefe650a04bba80fd942dbb3e45f69e0bbdff
svlandeg <sofie.vanlandeghem@gmail.com>
2020-10-15 12:45:56 +0200

    setup benchmarking project for different textcat architectures

---
+++ benchmarks/textcat_architectures/scripts/functions.py
@@ -0,0 +1,41 @@
+import sys
+from typing import IO, Tuple, Callable, Dict, Any, Optional
+import spacy
+from spacy.language import Language
+from pathlib import Path
+
+from spacy.training import console_logger
+
+
+@spacy.registry.loggers("my_custom_logger.v1")
+def custom_logger(log_path):
+    console = console_logger(progress_bar=False)
+
+    def setup_logger(
+        nlp: Language, stdout: IO = sys.stdout, stderr: IO = sys.stderr
+    ) -> Tuple[Callable, Callable]:
+        stdout.write(f"Logging to {log_path}\n")
+        log_file = Path(log_path).open("w", encoding="utf8")
+        log_file.write("step\t")
+        log_file.write("score\t")
+        for pipe in nlp.pipe_names:
+            log_file.write(f"loss_{pipe}\t")
+        log_file.write("\n")
+        console_log_step, console_finalize = console(nlp, stdout, stderr)
+
+        def log_step(info: Optional[Dict[str, Any]]):
+            console_log_step(info)
+            if info:
+                log_file.write(f"{info['step']}\t")
+                log_file.write(f"{info['score']}\t")
+                for pipe in nlp.pipe_names:
+                    log_file.write(f"{info['losses'][pipe]}\t")
+                log_file.write("\n")
+
+        def finalize():
+            console_finalize()
+            log_file.close()
+
+        return log_step, finalize
+
+    return setup_logger
