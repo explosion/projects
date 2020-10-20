fd17afe189d450006ac772c969586f0152e7f832
svlandeg <sofie.vanlandeghem@gmail.com>
2020-10-20 17:12:35 +0200

    further experiments

--- experimental/rel_component/scripts/rel_model.py
+++ experimental/rel_component/scripts/rel_model.py
@@ -54,13 +54,14 @@ def create_tensors() -> Callable[[List["Doc"], Callable, List[Floats2d], Ops], T
             result = []
             d = 0
             for i, shape in enumerate(shapes):
-                # TODO: make more efficient
+                # TODO: make more efficient / succint
                 d_tokvecs = ops.alloc2f(*shape)
                 row_dim = d_tokvecs.shape[1]
                 ents = candidates[i]
                 indices = {}
                 with numpy.printoptions(precision=2, suppress=True):
                     # print()
+                    # collect all relevant indices for each entity
                     for (ent1, ent2) in ents:
                         t1 = (ent1.start, ent1.end)
                         indices[t1] = indices.get(t1, [])
@@ -70,6 +71,7 @@ def create_tensors() -> Callable[[List["Doc"], Callable, List[Floats2d], Ops], T
                         indices[t2] = indices.get(t2, [])
                         indices[t2].append((d, row_dim, len(d_candidates[d])))
                         d += 1
+                    # for each entity, take the mean of its values
                     for token, sources in indices.items():
                         start, end = token
                         for source in sources:
@@ -102,7 +104,8 @@ def create_candidates(max_length: int) -> Callable[["Doc"], List[Tuple["Span", "
 
 @registry.architectures.register("rel_output_layer.v1")
 def create_layer(nI: int = None, nO: int = None) -> Model[Floats2d, Floats2d]:
-    return Linear(nO=nO, nI=nI)
+    with Model.define_operators({">>": chain}):
+        return Linear(nO=nO, nI=nI) >> Logistic()
 
 
 def forward(model, docs, is_train):
@@ -112,10 +115,6 @@ def forward(model, docs, is_train):
     get_candidates = model.attrs["get_candidates"]
     create_candidate_tensor = model.attrs["create_candidate_tensor"]
     output_layer = model.get_ref("output_layer")
-    for doc in docs:
-        print(doc.text)
-        print(tok2vec([doc], is_train=False))
-    print()
     tokvecs, bp_tokvecs = tok2vec(docs, is_train)
     print("tokvecs", tokvecs)
     cand_vectors, bp_cand = create_candidate_tensor(docs, get_candidates, tokvecs, model.ops)
@@ -134,26 +133,16 @@ def forward(model, docs, is_train):
 def init(
     model: Model, X: List["Doc"] = None, Y: Floats2d = None
 ) -> Model:
-    print()
-    print("INIT rel_model")
-    print("before")
     get_candidates = model.attrs["get_candidates"]
     create_candidate_tensor = model.attrs["create_candidate_tensor"]
     output_layer = model.get_ref("output_layer")
     tok2vec = model.get_ref("tok2vec")
     if X is not None:
         tok2vec.initialize(X=X)
-        for doc in X:
-            print(doc.text)
-            print(tok2vec([doc], is_train=False))
         tokvecs = tok2vec.predict(X)
         cand_vectors, _ = create_candidate_tensor(X, get_candidates, tokvecs, model.ops)
         output_layer.initialize(X=cand_vectors, Y=Y)
 
     if model.has_dim("nO") is None:
         model.set_dim("nO", output_layer.get_dim("nO"))
-    print("after")
-    for doc in X:
-        print(doc.text)
-        print(tok2vec([doc], is_train=False))
     return model
