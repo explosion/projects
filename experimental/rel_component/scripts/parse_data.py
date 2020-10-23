02e3b5eb686571ca19de47a5964a41b1ccb8c4e6
svlandeg <sofie.vanlandeghem@gmail.com>
2020-10-23 20:04:45 +0200

    adjust some settings

--- experimental/rel_component/scripts/parse_data.py
+++ experimental/rel_component/scripts/parse_data.py
@@ -10,8 +10,14 @@ from wasabi import Printer
 
 msg = Printer()
 
-ALL_LABELS = ["Pos-Reg", "Neg-Reg", "Reg", "Binds", "No-Effect"]
 SYMM_LABELS = ["Binds"]
+MAP_LABELS = {
+    "Pos-Reg": "Regulates",
+    "Neg-Reg": "Regulates",
+    "Reg": "Regulates",
+    "No-rel": "Regulates",
+    "Binds": "Binds",
+}
 
 
 def main(json_loc: Path, train_file: Path, dev_file: Path, test_file: Path):
@@ -40,7 +46,9 @@ def main(json_loc: Path, train_file: Path, dev_file: Path, test_file: Path):
                     spans = example["spans"]
                     entities = []
                     for span in spans:
-                        entity = doc.char_span(span["start"], span["end"], label=span["label"])
+                        entity = doc.char_span(
+                            span["start"], span["end"], label=span["label"]
+                        )
                         entities.append(entity)
                         span_starts.add(span["token_start"])
                     doc.ents = entities
@@ -55,8 +63,7 @@ def main(json_loc: Path, train_file: Path, dev_file: Path, test_file: Path):
                         start = relation["head_span"]["token_start"]
                         end = relation["child_span"]["token_start"]
                         label = relation["label"]
-                        if label == "No-rel":
-                            label = "No-Effect"
+                        label = MAP_LABELS[label]
                         rels[(start, end)][label] = 1.0
                         if label in SYMM_LABELS:
                             rels[(end, start)][label] = 1.0
@@ -64,14 +71,15 @@ def main(json_loc: Path, train_file: Path, dev_file: Path, test_file: Path):
                     # The annotation is complete, so fill in zero's where the data is missing
                     for x1 in span_starts:
                         for x2 in span_starts:
-                            for label in ALL_LABELS:
+                            for label in MAP_LABELS.values():
                                 if rels[(x1, x2)].get(label, None) is None:
                                     rels[(x1, x2)][label] = 0.0
                     doc._.rel = rels
 
                     # use the original PMID/PMCID to decide on train/dev/test split
                     article_id = example["meta"]["source"]
-                    article_id.replace("BioNLP 2011 Genia Shared Task, ", "")
+                    article_id = article_id.replace("BioNLP 2011 Genia Shared Task, ", "")
+                    article_id = article_id.replace(".txt", "")
                     article_id = article_id.split("-")[1]
                     if article_id.endswith("4"):
                         dev_ids.add(article_id)
@@ -83,19 +91,25 @@ def main(json_loc: Path, train_file: Path, dev_file: Path, test_file: Path):
                         train_ids.add(article_id)
                         train_docs.append(doc)
                 except KeyError as e:
-                    msg.fail(f"Skipping example: {example}")
+                    msg.fail(f"Skipping doc because of key error: {e} in {example['meta']['source']}")
 
     docbin = DocBin(docs=train_docs, store_user_data=True)
     docbin.to_disk(train_file)
-    msg.info(f"Wrote {len(train_docs)} training sentences from {len(train_ids)} articles to {train_file}")
+    msg.info(
+        f"Wrote {len(train_docs)} training sentences from {len(train_ids)} articles to {train_file}"
+    )
 
     docbin = DocBin(docs=dev_docs, store_user_data=True)
     docbin.to_disk(dev_file)
-    msg.info(f"Wrote {len(dev_docs)} dev sentences from {len(dev_ids)} articles to {dev_file}")
+    msg.info(
+        f"Wrote {len(dev_docs)} dev sentences from {len(dev_ids)} articles to {dev_file}"
+    )
 
     docbin = DocBin(docs=test_docs, store_user_data=True)
     docbin.to_disk(test_file)
-    msg.info(f"Wrote {len(test_docs)} test sentences from {len(test_ids)} articles to {test_file}")
+    msg.info(
+        f"Wrote {len(test_docs)} test sentences from {len(test_ids)} articles to {test_file}"
+    )
 
 
 if __name__ == "__main__":
