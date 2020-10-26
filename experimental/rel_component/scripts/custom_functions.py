5c76d44607bc190a58ad46932a68d10ff26b3466
svlandeg <sofie.vanlandeghem@gmail.com>
2020-10-26 13:52:54 +0100

    ensure tokenization is kept in pred documents

--- experimental/rel_component/scripts/custom_functions.py
+++ experimental/rel_component/scripts/custom_functions.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from typing import Iterable, Callable
 from spacy import registry
 from spacy.training import Example
-from spacy.tokens import DocBin
+from spacy.tokens import DocBin, Doc
 
 from scripts.rel_pipe import make_relation_extractor  # make the factory work
 from scripts.rel_model import create_relation_model, create_candidates, create_layer  # make the config work
@@ -15,10 +15,15 @@ def create_docbin_reader(file: Path) -> Callable[["Language"], Iterable[Example]
 
 
 def read_files(file: Path, nlp: "Language") -> Iterable[Example]:
-    # we add the gold GGP annotations to the "predictions" doc as we do not attempt to predict these
+    """Custom reader that keeps the tokenization of the gold data,
+    and also adds the gold GGP annotations as we do not attempt to predict these."""
     doc_bin = DocBin().from_disk(file)
     docs = doc_bin.get_docs(nlp.vocab)
     for gold in docs:
-        pred = nlp.make_doc(gold.text)
+        pred = Doc(
+            nlp.vocab,
+            words=[t.text for t in gold],
+            spaces=[t.whitespace_ for t in gold],
+        )
         pred.ents = gold.ents
         yield Example(pred, gold)
