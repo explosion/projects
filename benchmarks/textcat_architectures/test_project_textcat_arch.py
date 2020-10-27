ff415a7fdbf1c57145b51835e0c1868c70d423f0
svlandeg <sofie.vanlandeghem@gmail.com>
2020-10-27 01:14:45 +0100

    add install command

--- benchmarks/textcat_architectures/test_project_textcat_arch.py
+++ benchmarks/textcat_architectures/test_project_textcat_arch.py
@@ -3,7 +3,8 @@ from spacy.cli.project.assets import project_assets
 from pathlib import Path
 
 
-def test_benchmark_speed_project():
+def test_benchmark_textcat_project():
     root = Path(__file__).parent
     project_assets(root)
+    project_run(root, "install")
     project_run(root, "all")
