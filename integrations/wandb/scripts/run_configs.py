568d9c57189a65c3afe0dccc78786efc04e9bdd0
svlandeg <sofie.vanlandeghem@gmail.com>
2021-01-07 22:37:06 +0100

    update training call to latest v3 RC

--- integrations/wandb/scripts/run_configs.py
+++ integrations/wandb/scripts/run_configs.py
@@ -1,17 +1,23 @@
 import typer
 from pathlib import Path
 from spacy.cli.train import train
+from spacy.training.initialize import init_nlp
+from spacy.util import load_config
 
 
-def main(config_dir: Path, default_config: str, results_dir: Path):
+def main(config_dir: Path, results_dir: Path):
     """Run all config files instead of the default one.
     Ideally, these runs are parellellized instead of run in sequence."""
-    for config_file in config_dir.iterdir():
-        if config_file.name != default_config:
-            output_path = results_dir / config_file.stem
-            if not output_path.exists():
-                output_path.mkdir()
-            train(config_path=config_file, output_path=output_path)
+    if not config_dir.exists() or not config_dir.is_dir():
+        print(f"Could not read from folder {config_dir}")
+    for config_path in config_dir.iterdir():
+        print(f"Training on {config_path}")
+        config = load_config(config_path, interpolate=False)
+        nlp = init_nlp(config, use_gpu=False)
+        output_path = results_dir / config_path.stem
+        if not output_path.exists():
+            output_path.mkdir()
+        train(nlp, output_path, use_gpu=False)
 
 
 if __name__ == "__main__":
