26310add50118d31fcf3636d330e6c14dc56603e
Sofie Van Landeghem <svlandeg@users.noreply.github.com>
2021-01-14 12:14:49 +0100

    Update train call in W&B project (#24)
    * update training call to latest v3 RC
    
    * update readme
    
    * update import

--- integrations/wandb/scripts/customize_configs.py
+++ integrations/wandb/scripts/customize_configs.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from thinc.api import Config
 
 
-def main(config_dir: Path, default_config: str):
-    cfg = Config().from_disk(config_dir / default_config, interpolate=False)
+def main(config_dir: Path, default_config: Path):
+    cfg = Config().from_disk(default_config, interpolate=False)
     # hyperparameter grid search
     i = 0
     for depth in [2, 4]:
@@ -17,7 +17,7 @@ def main(config_dir: Path, default_config: str):
                     cfg["components"]["textcat"]["model"]["ngram_size"] = ngram
                     cfg["components"]["tok2vec"]["model"]["encode"]["depth"] = depth
                     cfg.to_disk(config_dir / f"config_{i}.cfg")
-    print(f"Saved {i} configs")
+    print(f"Saved {i} configs to {config_dir}")
 
 
 if __name__ == "__main__":
