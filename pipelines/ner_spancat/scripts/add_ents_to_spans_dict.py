0fcb0dae427567506e9f33501a715d1c7c645c51
Matthew Honnibal <honnibal+gh@gmail.com>
2021-01-18 12:59:34 +1100

    Add preprocess script

---
+++ pipelines/ner_spancat/scripts/add_ents_to_spans_dict.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import typer
+import spacy
+from spacy.tokens import DocBin
+
+
+def main(loc: Path, lang: str="id", span_key: str="spans"):
+    """
+    Set the NER data into the doc.spans, under a given key.
+
+    The SpanCategorizer component uses the doc.spans, so that it can work with
+    overlapping or nested annotations, which can't be represented on the
+    per-token level.
+    """
+    nlp = spacy.blank("id")
+    docbin = DocBin().from_disk(loc)
+    docs = list(docbin.get_docs(nlp.vocab))
+    for doc in docs:
+        doc.spans[span_key] = list(doc.ents)
+    DocBin(docs=docs).to_disk(loc)
+
+
+if __name__ == "__main__":
+    typer.run(main)
