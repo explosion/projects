ae1ac9e1e7ddedf78ea39e06315bb5eed35df45b
svlandeg <sofie.vanlandeghem@gmail.com>
2021-01-21 12:14:26 +0100

    xfail the REL project until the next RC of spaCy

--- tutorials/rel_component/test_project_rel.py
+++ tutorials/rel_component/test_project_rel.py
@@ -1,8 +1,10 @@
+import pytest
 from spacy.cli.project.run import project_run
 from spacy.cli.project.assets import project_assets
 from pathlib import Path
 
 
+@pytest.mark.xfail(reason='Requires spaCy v3.0.0rc4')
 def test_rel_project():
     root = Path(__file__).parent
     project_assets(root)
