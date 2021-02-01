0b9a46808049e3726a6488d6dc7a93cf9a86b411
svlandeg <sofie.vanlandeghem@gmail.com>
2021-02-01 20:15:53 +0100

    enable tests again

--- benchmarks/speed/test_project_speed_benchmark.py
+++ benchmarks/speed/test_project_speed_benchmark.py
@@ -1,11 +1,7 @@
-import pytest
 from spacy.cli.project.run import project_run
 from pathlib import Path
 
 
-@pytest.mark.skip(
-    reason="CLI is acting up - fixing it but skipping the test in the meantime."
-)
 def test_benchmark_speed_project():
     root = Path(__file__).parent
     # project_assets(root)   # there are currently no assets defined for this project
--- tutorials/nel_emerson/test_project_nel_emerson.py
+++ tutorials/nel_emerson/test_project_nel_emerson.py
@@ -1,12 +1,8 @@
-import pytest
 from spacy.cli.project.run import project_run
 from spacy.cli.project.assets import project_assets
 from pathlib import Path
 
 
-@pytest.mark.skip(
-    reason="CLI is acting up - fixing it but skipping the test in the meantime."
-)
 def test_nel_emerson_project():
     root = Path(__file__).parent
     project_assets(root)
