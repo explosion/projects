b3592d9e6dea361527ab8f4f3d080751f2470e29
Adriane Boyd <adrianeboyd@gmail.com>
2021-02-18 16:23:12 +0100

    Demo textcat project
    * Provided data is textcat_docs_issues data in a simpler JSONL format
    * Both `textcat` and `textcat_multilabel` data and configs are provided,
    the default is `textcat`

---
+++ pipelines/textcat_demo/scripts/convert.py
@@ -0,0 +1,23 @@
+"""Convert textcat annotation from JSONL to spaCy v3 .spacy format."""
+import srsly
+import typer
+import warnings
+from pathlib import Path
+
+import spacy
+from spacy.tokens import DocBin
+
+
+def convert(lang: str, input_path: Path, output_path: Path):
+    nlp = spacy.blank(lang)
+    docs = []
+    for line in srsly.read_jsonl(input_path):
+        doc = nlp.make_doc(line["text"])
+        doc.cats = line["cats"]
+        docs.append(doc)
+    db = DocBin(docs=docs)
+    db.to_disk(output_path)
+
+
+if __name__ == "__main__":
+    typer.run(convert)
