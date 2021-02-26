fa0b71bedfe9fb1609eda970d1f7494e60c5eed1
Adriane Boyd <adrianeboyd@gmail.com>
2021-02-26 17:01:49 +0100

    Remove download from all

--- pipelines/ner_demo_update/test_project_ner_demo_update.py
+++ pipelines/ner_demo_update/test_project_ner_demo_update.py
@@ -6,5 +6,6 @@ from pathlib import Path
 def test_ner_demo_update_project():
     root = Path(__file__).parent
     project_assets(root)
+    project_run(root, "download", capture=True)
     project_run(root, "all", capture=True)
     project_run(root, "package", capture=True)
