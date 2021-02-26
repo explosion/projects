36611fc0123870f301d0a183cf85515d92717d10
Adriane Boyd <adrianeboyd@gmail.com>
2021-02-26 11:20:50 +0100

    Add demo NER project that replaces in an existing pipeline
    Add a demo NER project that replaces the NER component in an existing
    pipeline, preserving/freezing all other components in the pipeline.

---
+++ pipelines/ner_demo_replace/scripts/create_config.py
@@ -0,0 +1,36 @@
+import typer
+from pathlib import Path
+
+import spacy
+from thinc.api import Config
+
+
+def create_config(model_name: str, component_to_replace: str, output_path: Path):
+    nlp = spacy.load(model_name)
+
+    # replace the component with a new default component
+    nlp.remove_pipe(component_to_replace)
+    nlp.add_pipe(component_to_replace)
+
+    # create a new config as a copy of the loaded pipeline's config
+    config = Config(nlp.config)
+
+    # source all components from the loaded pipeline and freeze all except the
+    # component to replace
+    config["training"]["frozen_components"] = []
+    for pipe_name in nlp.component_names:
+        config["components"][pipe_name] = {"source": model_name}
+        if pipe_name != component_to_replace:
+            config["training"]["frozen_components"].append(pipe_name)
+
+    # revert some custom training settings
+    default_config = spacy.blank(nlp.lang).config
+    config["corpora"]["train"]["augmenter"] = default_config["corpora"]["train"]["augmenter"]
+    config["training"]["logger"] = default_config["training"]["logger"]
+
+    # save the config
+    config.to_disk(output_path)
+
+
+if __name__ == "__main__":
+    typer.run(create_config)
