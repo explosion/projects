a249e44f4dfe60759d5f161e1617398b7b4bc8a5
Adriane Boyd <adrianeboyd@gmail.com>
2021-02-26 16:49:57 +0100

    Revert training and corpora blocks in modified configs

--- pipelines/ner_demo_update/scripts/create_config.py
+++ pipelines/ner_demo_update/scripts/create_config.py
@@ -11,6 +11,11 @@ def create_config(model_name: str, component_to_update: str, output_path: Path):
     # create a new config as a copy of the loaded pipeline's config
     config = Config(nlp.config)
 
+    # revert most training settings to the current defaults
+    default_config = spacy.blank(nlp.lang).config
+    config["corpora"] = default_config["corpora"]
+    config["training"] = default_config["training"]
+
     # source all components from the loaded pipeline and freeze all except the
     # component to update
     config["training"]["frozen_components"] = []
@@ -18,11 +23,6 @@ def create_config(model_name: str, component_to_update: str, output_path: Path):
         config["components"][pipe_name] = {"source": model_name}
         if pipe_name != component_to_update:
             config["training"]["frozen_components"].append(pipe_name)
-
-    # revert some custom training settings
-    default_config = spacy.blank(nlp.lang).config
-    config["corpora"]["train"]["augmenter"] = default_config["corpora"]["train"]["augmenter"]
-    config["training"]["logger"] = default_config["training"]["logger"]
 
     # save the config
     config.to_disk(output_path)
