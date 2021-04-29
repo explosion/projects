c1d86fa65b7cc2f3c09d342112844a401003df5c
Adriane Boyd <adrianeboyd@gmail.com>
2021-04-29 16:21:41 +0200

    Update NER demos for v3.0.6 (#54)
    * use `spacy.copy_from_base_model.v1`
    * use `replace_listeners` when sourcing the component that should be
    updated

--- pipelines/ner_demo_update/scripts/create_config.py
+++ pipelines/ner_demo_update/scripts/create_config.py
@@ -15,17 +15,30 @@ def create_config(model_name: str, component_to_update: str, output_path: Path):
     config["corpora"] = default_config["corpora"]
     config["training"] = default_config["training"]
 
-    # set the vectors if the loaded pipeline has vectors
-    if len(nlp.vocab.vectors) > 0:
-        config["paths"]["vectors"] = model_name
+    # copy tokenizer and vocab settings from the base model, which includes
+    # lookups (lexeme_norm) and vectors, so they don't need to be copied or
+    # initialized separately
+    config["initialize"]["before_init"] = {
+        "@callbacks": "spacy.copy_from_base_model.v1",
+        "tokenizer": model_name,
+        "vocab": model_name,
+    }
+    config["initialize"]["lookups"] = None
+    config["initialize"]["vectors"] = None
 
     # source all components from the loaded pipeline and freeze all except the
-    # component to update
+    # component to update; replace the listener for the component that is
+    # being updated so that it can be updated independently
     config["training"]["frozen_components"] = []
     for pipe_name in nlp.component_names:
-        config["components"][pipe_name] = {"source": model_name}
         if pipe_name != component_to_update:
+            config["components"][pipe_name] = {"source": model_name}
             config["training"]["frozen_components"].append(pipe_name)
+        else:
+            config["components"][pipe_name] = {
+                "source": model_name,
+                "replace_listeners": ["model.tok2vec"],
+            }
 
     # save the config
     config.to_disk(output_path)
