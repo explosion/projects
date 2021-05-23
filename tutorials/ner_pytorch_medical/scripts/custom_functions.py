9e0e58c565b09cfbdba5ccf145921b15caf1e475
Kabir Khan <kabir@subliminal.ai>
2021-05-23 13:52:33 -0700

    rm v3

--- tutorials/ner_pytorch_medical/scripts/custom_functions.py
+++ tutorials/ner_pytorch_medical/scripts/custom_functions.py
@@ -1,5 +1,2 @@
-# from scripts.torch_ner_model import build_torch_ner_model
-# from scripts.torch_ner_pipe import make_torch_entity_recognizer
-
-from scripts.torch_ner_model_v3 import build_torch_ner_model
-from scripts.torch_ner_pipe_v3 import make_torch_entity_recognizer
+from scripts.torch_ner_model import build_torch_ner_model
+from scripts.torch_ner_pipe import make_torch_entity_recognizer
