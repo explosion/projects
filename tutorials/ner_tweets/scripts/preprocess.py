393c16e123b8a301e6b7dec194ec2e8276e7afba
Lj Miranda <ljvmiranda@gmail.com>
2021-10-07 10:02:02 +0800

    Use a model immediately to include the entities

--- tutorials/ner_tweets/scripts/preprocess.py
+++ tutorials/ner_tweets/scripts/preprocess.py
@@ -4,19 +4,29 @@ import spacy
 import typer
 from spacy.tokens import DocBin
 from wasabi import msg
+from tqdm import tqdm
 
 
 ASSETS_DIR = Path(__file__).parent.parent / "assets"
 
 
-def convert_to_spacy(assets_dir: Path = ASSETS_DIR, lang: str = "en"):
-    """Convert raw text file of tweets into spaCy's binary format"""
-    nlp = spacy.blank(lang)
+def convert_to_spacy(assets_dir: Path = ASSETS_DIR, model: str = "en_core_web_lg"):
+    """Convert raw text file of tweets into spaCy's binary format
+
+    At the onset, we will already use the `en_core_web_lg` model to
+    store the detected entities in the DocBin.
+
+    assets_dir (Path): path to the assets directory for saving
+    model (str): the spaCy pretrained model to use
+    """
+    nlp = spacy.load(model)
     raw_text_fp = assets_dir / "raw.txt"
 
-    msg.text(f"Converting records to spacy format...")
     records = raw_text_fp.read_text().strip().split("\n")
-    docs = [nlp.make_doc(record) for record in records]
+    msg.text(f"Converting {len(records)} records to spacy format...")
+    with nlp.select_pipes(enable="ner"):  # we only need the NER component
+        msg.text(f"Using pipes: {nlp.pipe_names}")
+        docs = [nlp(record) for record in tqdm(records)]
 
     out_file = assets_dir / raw_text_fp.with_suffix(".spacy").parts[-1]
     out_data = DocBin(docs=docs).to_bytes()
