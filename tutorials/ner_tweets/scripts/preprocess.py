4bc57f365c7851aa337fb787be1f7f9847de114b
Lj Miranda <ljvmiranda@gmail.com>
2021-10-11 14:03:37 +0800

    Update preprocess commands for training and test

--- tutorials/ner_tweets/scripts/preprocess.py
+++ tutorials/ner_tweets/scripts/preprocess.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 import spacy
@@ -9,8 +10,11 @@ from tqdm import tqdm
 
 ASSETS_DIR = Path(__file__).parent.parent / "assets"
 
+app = typer.Typer()
 
-def convert_to_spacy(assets_dir: Path = ASSETS_DIR, model: str = "en_core_web_lg"):
+
+@app.command()
+def prepare_train_data(assets_dir: Path = ASSETS_DIR, model: str = "en_core_web_lg"):
     """Convert raw text file of tweets into spaCy's binary format
 
     At the onset, we will already use the `en_core_web_lg` model to
@@ -20,7 +24,7 @@ def convert_to_spacy(assets_dir: Path = ASSETS_DIR, model: str = "en_core_web_lg
     model (str): the spaCy pretrained model to use
     """
     nlp = spacy.load(model)
-    raw_text_fp = assets_dir / "raw.txt"
+    raw_text_fp = assets_dir / "train.txt"
 
     records = raw_text_fp.read_text().strip().split("\n")
     msg.text(f"Converting {len(records)} records to spacy format...")
@@ -35,5 +39,35 @@ def convert_to_spacy(assets_dir: Path = ASSETS_DIR, model: str = "en_core_web_lg
     msg.good(f"Done! Saved to {out_file}")
 
 
+@app.command()
+def prepare_test_data(assets_dir: Path = ASSETS_DIR):
+    """Convert raw text file into jsonl as Prodigy input
+
+    Using the test.txt causes some weird line breaks, probably due to the hashtag.
+    Standardizing them into Prodigy's preferred input format is much better.
+
+    assets_dir (Path): path to the assets directory for saving
+    """
+
+    raw_text_fp = assets_dir / "test.txt"
+    records = raw_text_fp.read_text().strip().split("\n")
+    msg.text(f"Converting {len(records)} records to JSONL format...")
+
+    out_file = assets_dir / raw_text_fp.with_suffix(".jsonl").parts[-1]
+
+    with out_file.open("w") as fp:
+        for record in records:
+            entry = {"text": record}
+            json.dump(entry, fp)
+            fp.write("\n")
+
+
+@app.command()
+def prepare_datasets():
+    """Prepare both training and test datasets to suitable formats"""
+    prepare_train_data()
+    prepare_test_data()
+
+
 if __name__ == "__main__":
-    typer.run(convert_to_spacy)
+    app()
