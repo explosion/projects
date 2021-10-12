3ce1dc2c14f5828a05ad75854efb86347e8787a7
Lj Miranda <ljvmiranda@gmail.com>
2021-10-12 12:36:21 +0800

    Simplify the preprocessing step further

--- tutorials/ner_tweets/scripts/preprocess.py
+++ tutorials/ner_tweets/scripts/preprocess.py
@@ -1,73 +1,40 @@
-import json
 from pathlib import Path
 
 import spacy
+import srsly
 import typer
-from spacy.tokens import DocBin
-from wasabi import msg
+from spacy.tokens import Doc, DocBin
+from spacy.util import get_words_and_spaces
 from tqdm import tqdm
+from wasabi import msg
 
 
-ASSETS_DIR = Path(__file__).parent.parent / "assets"
+def main(
+    input_path: Path = typer.Argument(..., exists=True, dir_okay=False),
+    output_path: Path = typer.Argument(..., dir_okay=False),
+    text_only: bool = False,
+):
+    """Preprocess JSONL files into the .spacy filetype"""
+    nlp = spacy.blank("en")
+    doc_bin = DocBin(attrs=["ENT_IOB", "ENT_TYPE"])  # we're just concerned with NER
 
-app = typer.Typer()
-
-
-@app.command()
-def prepare_train_data(assets_dir: Path = ASSETS_DIR, model: str = "en_core_web_lg"):
-    """Convert raw text file of tweets into spaCy's binary format
-
-    At the onset, we will already use the `en_core_web_lg` model to
-    store the detected entities in the DocBin.
-
-    assets_dir (Path): path to the assets directory for saving
-    model (str): the spaCy pretrained model to use
-    """
-    nlp = spacy.load(model)
-    raw_text_fp = assets_dir / "train.txt"
-
-    records = raw_text_fp.read_text().strip().split("\n")
-    msg.text(f"Converting {len(records)} records to spacy format...")
-    with nlp.select_pipes(enable="ner"):  # we only need the NER component
-        msg.text(f"Using pipes: {nlp.pipe_names}")
-        docs = [nlp(record) for record in tqdm(records)]
-
-    out_file = assets_dir / raw_text_fp.with_suffix(".spacy").parts[-1]
-    out_data = DocBin(docs=docs).to_bytes()
-    with out_file.open("wb") as fp:
-        fp.write(out_data)
-    msg.good(f"Done! Saved to {out_file}")
-
-
-@app.command()
-def prepare_test_data(assets_dir: Path = ASSETS_DIR):
-    """Convert raw text file into jsonl as Prodigy input
-
-    Using the test.txt causes some weird line breaks, probably due to the hashtag.
-    Standardizing them into Prodigy's preferred input format is much better.
-
-    assets_dir (Path): path to the assets directory for saving
-    """
-
-    raw_text_fp = assets_dir / "test.txt"
-    records = raw_text_fp.read_text().strip().split("\n")
-    msg.text(f"Converting {len(records)} records to JSONL format...")
-
-    out_file = assets_dir / raw_text_fp.with_suffix(".jsonl").parts[-1]
-
-    with out_file.open("w") as fp:
-        for record in records:
-            entry = {"text": record}
-            json.dump(entry, fp)
-            fp.write("\n")
-
-
-@app.command()
-def prepare_datasets():
-    """Prepare both training and test datasets to suitable formats"""
-    prepare_train_data()
-    prepare_test_data()
+    for record in tqdm(srsly.read_jsonl(input_path)):
+        if text_only:
+            doc = nlp(record["text"])
+        else:
+            if record["answer"] != "accept":
+                continue
+            tokens = [token["text"] for token in record["tokens"]]
+            words, spaces = get_words_and_spaces(tokens, record["text"])
+            doc = Doc(nlp.vocab, words=words, spaces=spaces)
+            doc.ents = [
+                doc.char_span(s["start"], s["end"], label=s["label"])
+                for s in record.get("spans", [])
+            ]
+        doc_bin.add(doc)
+    doc_bin.to_disk(output_path)
+    msg.good(f"Processed {len(doc_bin)} documents: {output_path.name}")
 
 
 if __name__ == "__main__":
-    app()
+    typer.run(main)
