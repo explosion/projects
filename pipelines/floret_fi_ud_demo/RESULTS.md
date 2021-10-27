90eb681a106e63aadaa151bbb7a9077af6feb246
Adriane Boyd <adrianeboyd@gmail.com>
2021-10-27 09:45:13 +0200

    Add floret Finnish UD+NER demo

---
+++ pipelines/floret_fi_ud_demo/RESULTS.md
@@ -0,0 +1,11 @@
+## Results
+
+With 1M (2.6G) tokenized training texts and 50K 300-dim vectors, ~300K keys for
+the standard vectors:
+
+| Vectors                                      |  TAG |  POS | DEP UAS | DEP LAS | NER F |
+| -------------------------------------------- | ---: | ---: | ------: | ------: | ----: |
+| none                                         | 93.3 | 92.3 |    79.7 |    72.8 |  61.0 |
+| standard (pruned: 50K vectors for 300K keys) | 95.9 | 94.7 |    83.3 |    77.9 |  68.5 |
+| standard (unpruned: 300K vectors/keys)       | 96.0 | 95.0 |    83.8 |    78.4 |  69.1 |
+| floret (minn 2, maxn 3; 50K vectors, no OOV) | 96.6 | 95.5 |    83.5 |    78.5 |  70.9 |
---
+++ pipelines/floret_fi_ud_demo/scripts/tokenize_dataset.py
@@ -0,0 +1,42 @@
+import re
+import spacy
+import typer
+from itertools import islice
+from pathlib import Path
+from datasets import load_dataset
+
+
+def main(
+    lang: str,
+    oscar_dataset: str,
+    max_texts: int,
+    output_file: Path,
+    n_process: int = 8,
+    batch_size: int = 100,
+):
+    if lang == "ko":
+        nlp = spacy.blank(
+            "ko", config={"nlp": {"tokenizer": {"@tokenizers": "spacy.Tokenizer.v1"}}}
+        )
+    elif lang == "zh":
+        nlp = spacy.blank("zh", config={"nlp": {"tokenizer": {"segmenter": "pkuseg"}}})
+        nlp.tokenizer.initialize(pkuseg_model="spacy_ontonotes")
+    else:
+        nlp = spacy.blank(lang)
+
+    nlp.add_pipe("sentencizer")
+    nlp.max_length = 10 ** 8
+
+    dataset = load_dataset("oscar", oscar_dataset, split="train", streaming=True)
+    with open(output_file, "w") as output_fileh:
+        texts = (
+            re.sub("\s+", " ", line["text"].strip())
+            for line in islice(iter(dataset), max_texts)
+        )
+        for doc in nlp.pipe(texts, n_process=n_process, batch_size=batch_size):
+            for sent in doc.sents:
+                output_fileh.write(" ".join([t.text for t in sent]) + "\n")
+
+
+if __name__ == "__main__":
+    typer.run(main)
