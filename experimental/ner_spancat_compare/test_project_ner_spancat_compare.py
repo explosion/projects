6135a53c9504a6b886454aa408a8a93c0cb9c1c4
Lj Miranda <ljvmiranda@gmail.com>
2021-12-03 17:03:40 +0800

    Add test to run the whole project

--- experimental/ner_spancat_compare/test_project_ner_spancat_compare.py
+++ experimental/ner_spancat_compare/test_project_ner_spancat_compare.py
@@ -1,4 +1,9 @@
+from pathlib import Path
+
 import pytest
+from spacy.cli.project.assets import project_assets
+from spacy.cli.project.run import project_run
+
 from scripts.preprocess import _get_contiguous_tokens
 
 
@@ -13,3 +18,10 @@ from scripts.preprocess import _get_contiguous_tokens
 def test_contiguous_tokens_generator(labels, span):
     """Test if the method for generating contiguous tokens from EBM-NLP works correctly"""
     assert set(_get_contiguous_tokens(labels)) == set(span)
+
+
+def test_ner_spancat_compare_project():
+    root = Path(__file__).parent
+    project_assets(root)
+    # TODO: Uncomment once workflow has been implemented
+    # project_run(root, "all", capture=True)
