9cf193aaae2fed4185a7d27340c5a826e7d31c3b
Adriane Boyd <adrianeboyd@gmail.com>
2021-12-14 16:35:12 +0100

    Update UD benchmark project (#89)
    * Working on updating UD benchmarks
    
    * Update README
    
    * Initialize transformer labels from gold tokenization
    
    * Add mixed_precision option
    
    * Update init labels docs
    
    * Add sents_per_text option to split up test texts on GPU
    
    * Updates for spacy-experimental, etc.
    
    * Use `spacy-experimental`
    * Move the lemmatizer into the transformer pipeline
    * Assume the parser init issue is fixed
    * Assume the package int label issue is fixed
    
    * Update description
    
    * Update README
    
    * Update README
    
    * Switch back to UD_English-EWT
    
    * Update spacy-experimental requirement
    
    * Add senter to default pipeline
    
    * Evaluate with and without senter
    * Also export evaluation metrics as JSON
    
    * Evaluate on senter in all, update package to copy license
    
    * Add spacy version
    
    * Format
    
    * Switch back to unnamed tempfile
    
    * Add tagger neg_prefix
    
    * Format
    
    * Lint
    
    * Fix optional evaluate command
    
    * Add spacy-transformers to requirements

--- benchmarks/ud_benchmark/README.md
+++ benchmarks/ud_benchmark/README.md
@@ -2,7 +2,7 @@
 
 # 🪐 spaCy Project: Universal Dependencies v2.5 Benchmarks
 
-This project template lets you train a part-of-speech tagger, a morphologizer and a dependency parser from a [Universal Dependencies](https://universaldependencies.org/) corpus (v2.5). It takes care of downloading the treebank, converting it to spaCy's format and training and evaluating the model. The template uses the [`UD_English-EWT`](https://github.com/UniversalDependencies/UD_English-EWT) treebank by default, but you can swap it out for any other available treebank. Just make sure to adjust the `ud_treebank`, `ud_prefix` and `spacy_lang` settings in the config. Use `xx` (multi-language) for `spacy_lang` if no language-specific tokenizer is available in spaCy.
+This project template lets you train a spaCy pipeline on any [Universal Dependencies](https://universaldependencies.org/) corpus (v2.5) for benchmarking purposes. The pipeline includes an experimental trainable tokenizer, an experimental edit tree lemmatizer, and the standard spaCy tagger, morphologizer and dependency parser components. The CoNLL 2018 evaluation script is used to evaluate the pipeline. The template uses the [`UD_English-EWT`](https://github.com/UniversalDependencies/UD_English-EWT) treebank by default, but you can swap it out for any other available treebank. Just make sure to adjust the `ud_treebank` and `spacy_lang` settings in the config. Use `xx` (multi-language) for `spacy_lang` if a particular language is not supported by spaCy. The tokenizer in particular is only intended for use in this generic benchmarking setup. It is not optimized for speed and it does not perform particularly well for languages without space-separated tokens. In production, custom rules for spaCy's rule-based tokenizer or a language-specific word segmenter such as jieba for Chinese or sudachipy for Japanese would be recommended instead.
 
 ## 📋 project.yml
 
@@ -20,10 +20,11 @@ Commands are only re-run if their inputs have changed.
 | --- | --- |
 | `extract` | Extract the data |
 | `convert` | Convert the data to spaCy's format |
-| `generate-config` | Generate config |
-| `train` | Train without vectors |
-| `train-with-vectors` | Train with vectors |
+| `train-tokenizer` | Train tokenizer |
+| `train-transformer` | Train transformer |
+| `assemble` | Assemble full pipeline |
 | `evaluate` | Evaluate on the test data and save the metrics |
+| `evaluate-with-senter` | Evaluate on the test data and save the metrics |
 | `package` | Package the trained model so it can be installed |
 | `clean` | Remove intermediate files |
 
@@ -36,7 +37,7 @@ inputs have changed.
 
 | Workflow | Steps |
 | --- | --- |
-| `all` | `extract` &rarr; `convert` &rarr; `generate-config` &rarr; `train` &rarr; `evaluate` &rarr; `package` |
+| `all` | `extract` &rarr; `convert` &rarr; `train-tokenizer` &rarr; `train-transformer` &rarr; `assemble` &rarr; `evaluate` &rarr; `evaluate-with-senter` &rarr; `package` |
 
 ### 🗂 Assets
 
--- benchmarks/ud_benchmark/scripts/evaluate.py
+++ benchmarks/ud_benchmark/scripts/evaluate.py
@@ -1,30 +1,77 @@
-import spacy
+from typing import Optional, List
+import glob
+from pathlib import Path
 import re
 import tempfile
 import typer
+import srsly
+import spacy
+from thinc.api import set_gpu_allocator
 from conll18_ud_eval import load_conllu, evaluate
-from typing import Optional, List
-import code
 
 
-def main(model: str, gold_conllu: str, text: Optional[str] = None, output: Optional[str] = None, gpu_id: int = -1):
+def main(
+    model: str,
+    gold_dir: Path,
+    output: Optional[str] = None,
+    gpu_id: int = -1,
+    batch_size: int = 64,
+    sents_per_text: int = -1,
+    enable_senter: bool = False,
+):
+    test_txt_file = None
+    test_txt_files = glob.glob(str(gold_dir.resolve()) + "/*test.txt")
+    if len(test_txt_files) > 0:
+        test_txt_file = test_txt_files[0]
+    test_conllu_file = None
+    test_conllu_files = glob.glob(str(gold_dir.resolve()) + "/*test.conllu")
+    if len(test_conllu_files) > 0:
+        test_conllu_file = test_conllu_files[0]
+
+    # if raw text input is provided, load texts from plain text file
+    if test_txt_file:
+        with open(test_txt_file) as fileh:
+            content = fileh.read()
+            texts = [
+                re.sub(r"\s+", " ", text.replace("\n", " ").strip())
+                for text in content.split("\n\n")
+            ]
+    # otherwise generate raw text input from gold CoNLL-U file
+    elif test_conllu_file:
+        texts = gold_to_texts(test_conllu_file)
+    else:
+        raise ValueError("No test.txt or test.conllu files found in", gold_dir)
+
+    # if desired (for GPU), break up very long texts
+    if sents_per_text > 0:
+        split_texts = []
+        sentencizer_nlp = spacy.blank("xx")
+        sentencizer_nlp.max_length = max(len(text) + 1 for text in texts)
+        sentencizer_nlp.add_pipe("sentencizer")
+        for doc in sentencizer_nlp.pipe(texts):
+            sents = list(doc.sents)
+            for i in range(0, len(sents), sents_per_text):
+                if len(sents) < i + sents_per_text:
+                    end_t = len(doc)
+                else:
+                    end_t = sents[i + sents_per_text][0].i
+                split_texts.append(doc[sents[i][0].i : end_t].text)
+    else:
+        split_texts = texts
+
     if gpu_id >= 0:
         spacy.require_gpu(gpu_id)
+        set_gpu_allocator("pytorch")
 
     # load model from model name or path
     nlp = spacy.load(model)
-
-    # if raw text input is provided, load texts from plain text file
-    if text:
-        with open(text) as fileh:
-            content = fileh.read()
-            texts = [text.replace("\n", " ").strip() for text in content.split("\n\n")]
-    # otherwise generate raw text input from gold CoNLL-U file
+    if enable_senter:
+        nlp.enable_pipe("senter")
     else:
-        texts = gold_to_texts(gold_conllu)
+        nlp.disable_pipe("senter")
 
-    # apply model to texts
-    docs = nlp.pipe(texts)
+    # apply model to texts (batch_size=1 is slow, but reduces chance of OOM)
+    docs = nlp.pipe(split_texts, batch_size=1)
 
     # load the system CoNLL-U predictions by creating a temporary CoNLL-U
     # output file
@@ -32,9 +79,8 @@ def main(model: str, gold_conllu: str, text: Optional[str] = None, output: Optio
     for doc in docs:
         for sent in doc.sents:
             output_lines.append("# text = " + sent.text + "\n")
+            assert all(not token.is_space for token in doc)
             for i, token in enumerate(sent):
-                if token.is_space:
-                    continue
                 cols = ["_"] * 10
                 cols[0] = str(i + 1)
                 cols[1] = token.text
@@ -46,7 +92,11 @@ def main(model: str, gold_conllu: str, text: Optional[str] = None, output: Optio
                     cols[4] = token.tag_
                 if str(token.morph):
                     cols[5] = str(token.morph)
-                cols[6] = "0" if token.head.i == token.i else str(token.head.i + 1 - sent[0].i)
+                cols[6] = (
+                    "0"
+                    if token.head.i == token.i
+                    else str(token.head.i + 1 - sent[0].i)
+                )
                 cols[7] = "root" if token.dep_ == "ROOT" else token.dep_
                 if not token.whitespace_:
                     cols[9] = "SpaceAfter=No"
@@ -59,7 +109,7 @@ def main(model: str, gold_conllu: str, text: Optional[str] = None, output: Optio
         pred_corpus = load_conllu(fileh)
 
     # load the gold CoNLL-U file
-    with open(gold_conllu) as fileh:
+    with open(test_conllu_file) as fileh:
         gold_corpus = load_conllu(fileh)
 
     # run the CoNLL 2018 shared task evaluation
@@ -68,8 +118,17 @@ def main(model: str, gold_conllu: str, text: Optional[str] = None, output: Optio
     # output evaluation to output file or stdout
     evaluation_table = format_evaluation(scores)
     if output:
-        with open(output, "w") as fileh:
+        with open(output + ".txt", "w") as fileh:
             fileh.write(evaluation_table)
+        scores_dict = {}
+        for key, score in scores.items():
+            scores_dict[key] = {
+                "precision": score.precision,
+                "recall": score.recall,
+                "f1": score.f1,
+                "aligned_accuracy": score.aligned_accuracy,
+            }
+        srsly.write_json(output + ".json", scores_dict)
     else:
         print("".join(evaluation_table))
 
@@ -80,14 +139,32 @@ def format_evaluation(evaluation) -> List[str]:
     lines = []
     lines.append("Metric     | Precision |    Recall |  F1 Score | AligndAcc")
     lines.append("-----------+-----------+-----------+-----------+-----------")
-    for metric in["Tokens", "Sentences", "Words", "UPOS", "XPOS", "UFeats", "AllTags", "Lemmas", "UAS", "LAS", "CLAS", "MLAS", "BLEX"]:
-        lines.append("{:11}|{:10.2f} |{:10.2f} |{:10.2f} |{}".format(
-            metric,
-            100 * evaluation[metric].precision,
-            100 * evaluation[metric].recall,
-            100 * evaluation[metric].f1,
-            "{:10.2f}".format(100 * evaluation[metric].aligned_accuracy) if evaluation[metric].aligned_accuracy is not None else ""
-        ))
+    for metric in [
+        "Tokens",
+        "Sentences",
+        "Words",
+        "UPOS",
+        "XPOS",
+        "UFeats",
+        "AllTags",
+        "Lemmas",
+        "UAS",
+        "LAS",
+        "CLAS",
+        "MLAS",
+        "BLEX",
+    ]:
+        lines.append(
+            "{:11}|{:10.2f} |{:10.2f} |{:10.2f} |{}".format(
+                metric,
+                100 * evaluation[metric].precision,
+                100 * evaluation[metric].recall,
+                100 * evaluation[metric].f1,
+                "{:10.2f}".format(100 * evaluation[metric].aligned_accuracy)
+                if evaluation[metric].aligned_accuracy is not None
+                else "",
+            )
+        )
     return "\n".join(lines)
 
 
@@ -102,14 +179,14 @@ def gold_to_texts(gold_conllu: str) -> List[str]:
         for line in fileh:
             line = line.strip()
             if text and (line.startswith("# newdoc") or line.startswith("# newpar")):
-                texts.append(re.sub("\s+", " ", text.strip()))
+                texts.append(re.sub(r"\s+", " ", text.strip()))
                 text = ""
             if line.startswith("# text = "):
                 text += line.replace("# text = ", "")
             if line == "":
                 cols = prev_line.split("\t")
                 if len(cols) >= 10:
-                    if not "SpaceAfter=No" in cols[9]:
+                    if "SpaceAfter=No" not in cols[9]:
                         text += " "
             prev_line = line
         if text:
