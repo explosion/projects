6e76750a6570f72f49dc75da0638e81ae3a09de8
Raphael Mitsch <r.mitsch@outlook.com>
2022-06-07 09:51:13 +0200

    NEL benchmark: fix cleaning script.

--- benchmarks/nel/scripts/datasets/reddit.py
+++ benchmarks/nel/scripts/datasets/reddit.py
@@ -124,7 +124,6 @@ class RedditDataset(Dataset):
 
     def clean_assets(self) -> None:
         to_remove = {
-            "bronze_post_annotations.tsv": {},
             "bronze_comment_annotations.tsv": {}
         }
         to_replace = {
@@ -132,19 +131,13 @@ class RedditDataset(Dataset):
                 "`` How to Lose a Guy in 10 Days": "How to Lose a Guy in 10 Days",
                 "\"How to Lose a Guy in 10 Days": "\"How to Lose a Guy in 10 Days\"",
                 "Money\t325\t330\tmoney\n": "Money\t323\t328\tmoney\n"
-            },
-            "bronze_post_annotations.tsv": {
-                # "8qntul\tsports\tad\tAdvertising\t56\t58\tparade\n": "8qntul\tsports\tad\tParade\t56\t58\tparade\n",
-                # "dtjq8bn\tpolitics\tGOOD\tGood\t102\t106\tGOOD\n": "dtjq8bn\tpolitics\tGOOD\tGood\t94\t98\tGOOD\n"
             }
         }
 
         for file_name in to_replace:
             for line in fileinput.input(self._paths["assets"] / file_name, inplace=True):
                 if line not in to_remove[file_name]:
-                    new_line = line
                     for snippet in to_replace[file_name]:
                         if snippet in line:
-                            new_line = line.replace(snippet, to_replace[file_name][snippet])
-                            break
-                    print(new_line, end='')
+                            line = line.replace(snippet, to_replace[file_name][snippet])
+                    print(line, end='')
