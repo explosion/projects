9cd808df9c5fc02003641c656f71c1be90c5fae6
Raphael Mitsch <r.mitsch@outlook.com>
2022-07-05 17:43:40 +0200

    Consider second- instead of first-level directories for test runs.

--- run-tests.sh
+++ run-tests.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# Only run tests in top-level directories that have been changed in the last commit.
+# Only run tests in second-level directories that have been changed in the last commit.
 mapfile -t dirs < <( git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g')
 declare -a tested_dirs=()
 for dir in "${dirs[@]}"
 do
-  top_level_dir=$(echo "$dir" | awk -F/ '{print FS $1}')
-  top_level_dir="${top_level_dir:1}"
-  if [[ ! " ${tested_dirs[*]} " =~ " ${top_level_dir} " ]]; then
-    tested_dirs+=($top_level_dir)
-    python -m pytest -s $top_level_dir
+  second_level_dir=$(echo "$dir" | awk -F/ '{print FS $2}')
+  second_level_dir="${second_level_dir:1}"
+
+  if [ ! -z "$second_level_dir" -a "$second_level_dir" != " " ]; then
+    if [[ ! " ${tested_dirs[*]} " =~ " ${second_level_dir} " ]]; then
+      tested_dirs+=($second_level_dir)
+      python -m pytest -s $second_level_dir
+    fi
   fi
+
 done
