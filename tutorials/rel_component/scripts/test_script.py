cd56cef7c15d8da5b4b8511a4898d72a4b43a06a
Karthik Ragunath Ananda Kumar <kkumar@ancestry.com>
2022-07-06 17:34:50 -0600

    debugging

---
+++ tutorials/rel_component/scripts/test_script.py
@@ -0,0 +1,19 @@
+import random
+import typer
+from pathlib import Path
+import spacy
+from spacy.tokens import DocBin, Doc
+from spacy.training.example import Example
+
+# make the factory work
+from rel_pipe import make_relation_extractor, score_relations
+
+# make the config work
+from rel_model import create_relation_model, create_classification_layer, create_instances, create_tensors
+
+
+def main(trained_pipeline: Path):
+    nlp = spacy.load(trained_pipeline)
+
+if __name__ == "__main__":
+    typer.run(main)
