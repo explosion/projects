7c364f599ef624d385ff699c2d16d4d025194e0e
Karthik Ragunath Ananda Kumar <kkumar@ancestry.com>
2022-07-06 17:46:47 -0600

    debugging

--- tutorials/rel_component/scripts/test_script.py
+++ tutorials/rel_component/scripts/test_script.py
@@ -12,8 +12,11 @@ from rel_pipe import make_relation_extractor, score_relations
 from rel_model import create_relation_model, create_classification_layer, create_instances, create_tensors
 
 
-def main(trained_pipeline: Path):
+def main(trained_pipeline: Path, input_str: str):
     nlp = spacy.load(trained_pipeline)
+    print("Pipe Names:", nlp.pipe_names)
+    print("Pipeline Components:", nlp.pipeline)
+    print("Input String:", input_str)
 
 if __name__ == "__main__":
     typer.run(main)
