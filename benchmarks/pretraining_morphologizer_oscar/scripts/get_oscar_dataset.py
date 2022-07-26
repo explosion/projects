857db3238882f030505e27d8297df506af2a28c3
thomashacker <EdwardSchmuhl@web.de>
2022-07-26 17:16:10 +0200

    add commands to project yml

--- benchmarks/pretraining_morphologizer_oscar/scripts/get_oscar_dataset.py
+++ benchmarks/pretraining_morphologizer_oscar/scripts/get_oscar_dataset.py
@@ -4,6 +4,7 @@ import srsly
 from datasets import load_dataset
 from itertools import islice
 from wasabi import msg
+from tqdm import tqdm
 
 
 def main(max_texts: int, output_path: Path):
@@ -13,9 +14,20 @@ def main(max_texts: int, output_path: Path):
     dataset = load_dataset(
         "oscar", "unshuffled_deduplicated_en", split="train", streaming=True
     )
-    data = [line for line in islice(iter(dataset), max_texts)]
+    data = []
+    text_length = 0
+    for line in tqdm(
+        islice(iter(dataset), max_texts),
+        total=max_texts,
+        desc="Downloading OSCAR extract",
+    ):
+        data.append(line)
+        text_length += len(line["text"]) - line["text"].count(" ")
     srsly.write_jsonl(output_path, data)
-    msg.good(f"Saved data entries to {output_path}")
+    msg.info(
+        f"Downloaded extract contains about {int(round(text_length/1000000,0))} million characters"
+    )
+    msg.good(f"Saved to {output_path}")
 
 
 if __name__ == "__main__":
