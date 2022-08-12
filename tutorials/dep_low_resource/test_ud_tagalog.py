6a1f58642916aa53c221df5833e3e4858ae0800f
Lj Miranda <ljvmiranda@gmail.com>
2022-08-12 10:41:23 +0800

    Rename project test into something generic

--- tutorials/dep_low_resource/test_ud_tagalog.py
+++ tutorials/dep_low_resource/test_ud_tagalog.py
@@ -4,7 +4,7 @@ from spacy.cli.project.assets import project_assets
 from spacy.cli.project.run import project_run
 
 
-def test_ud_tagalog_project():
+def test_dep_low_resource_project():
     root = Path(__file__).parent
     project_assets(root)
     project_run(root, "all", overrides={"vars.n_folds": 1, "vars.gpu": 0})
