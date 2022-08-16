039a42a571272080344b4b45e2015f49288f4209
Lj Miranda <ljvmiranda@gmail.com>
2022-08-16 09:22:21 +0800

    Set capture to True to see build errors

--- tutorials/parser_low_resource/test_parser_low_resource.py
+++ tutorials/parser_low_resource/test_parser_low_resource.py
@@ -7,4 +7,4 @@ from spacy.cli.project.run import project_run
 def test_parser_low_resource_project():
     root = Path(__file__).parent
     project_assets(root)
-    project_run(root, "all", overrides={"vars.n_folds": 1, "vars.gpu_id": -1})
+    project_run(root, "all", overrides={"vars.n_folds": 1, "vars.gpu_id": -1}, capture=True)
