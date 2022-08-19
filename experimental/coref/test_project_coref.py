b05b893177375b46d373a2bdb7e977b0aa689200
Paul O'Leary McCann <polm@dampfkraft.com>
2022-08-19 20:12:30 +0900

    Add test script for CI

---
+++ experimental/coref/test_project_coref.py
@@ -0,0 +1,11 @@
+from spacy.cli.project.run import project_run
+from spacy.cli.project.assets import project_assets
+from pathlib import Path
+
+
+def test_experimental_coref():
+    root = Path(__file__).parent
+    # provide a placeholder value here, since we won't have real OntoNotes
+    overrides = {"vars.ontonotes": "assets"}
+    project_assets(root, overrides=overrides, extra=True)
+    project_run(root, "ci-test")
