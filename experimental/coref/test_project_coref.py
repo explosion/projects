74e460a821fe52844eb7ea280f3c0d29a191f1c4
Paul O'Leary McCann <polm@dampfkraft.com>
2022-08-19 20:23:49 +0900

    Don't use GPU in ci test

--- experimental/coref/test_project_coref.py
+++ experimental/coref/test_project_coref.py
@@ -6,6 +6,6 @@ from pathlib import Path
 def test_experimental_coref():
     root = Path(__file__).parent
     # provide a placeholder value here, since we won't have real OntoNotes
-    overrides = {"vars.ontonotes": "assets"}
+    overrides = {"vars.ontonotes": "assets", "vars.gpu": -1}
     project_assets(root, overrides=overrides, extra=True)
     project_run(root, "ci-test")
