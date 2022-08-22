15e0491ce68baf55bdc2ee168ad9b1fabe4d7840
Lj Miranda <12949683+ljvmiranda921@users.noreply.github.com>
2022-08-22 22:24:44 +0800

    Add sample project on a low-resource language (#122)
    * Add initial project structure
    
    * Parametrize the number of folds
    
    * Add config
    
    * Make kfold.py highly configurable
    
    * Add docstring
    
    * Add test to ensure project runs
    
    * Rename project test into something generic
    
    * Update tutorials readme
    
    * Update score report on README
    
    * Remove inter-treebank results
    
    * Use gpu_id instead of gpu
    
    * Rename test runner into project name
    
    * Rename project folder into parser_low_resource
    
    * Rename test file to component name
    
    * Update tutorials/parser_low_resource/test_parser_low_resource.py
    
    Co-authored-by: Adriane Boyd <adrianeboyd@gmail.com>
    
    * Fix gpu_id
    
    * Set capture to True to see build errors
    
    * Fix how folds are computed
    
    * Remove breakpoint
    
    * Fix output path saving for metrics
    
    * Update README
    
    * Update tutorials/parser_low_resource/test_parser_low_resource.py
    
    Co-authored-by: Adriane Boyd <adrianeboyd@gmail.com>
    
    * Convert to raw URLs
    
    * Include scripts.kfold to deps
    
    To retrigger execution if the scripts are changed but nothing else.
    
    * Add metrics to clean command
    
    Co-authored-by: Adriane Boyd <adrianeboyd@gmail.com>

--- tutorials/README.md
+++ tutorials/README.md
@@ -1,6 +1,6 @@
 <a href="https://explosion.ai"><img src="https://explosion.ai/assets/img/logo.svg" width="125" height="125" align="right" /></a>
 
-# 🪐 Project Templates: Tutorials (11)
+# 🪐 Project Templates: Tutorials (12)
 
 | Template | Description |
 | --- | --- |
@@ -11,6 +11,7 @@
 | [`ner_food_ingredients`](ner_food_ingredients) | Analyzing how mentions of ingredients change over time (Named Entity Recognition) |
 | [`ner_pytorch_medical`](ner_pytorch_medical) | Detecting entities in Medical Records with PyTorch |
 | [`ner_tweets`](ner_tweets) | Detecting people entities in tweets (Named Entity Recognition) |
+| [`parser_low_resource`](parser_low_resource) | Training a POS tagger and dependency parser for a low-resource language |
 | [`rel_component`](rel_component) | Example project of creating a novel nlp component to do relation extraction from scratch. |
 | [`textcat_docs_issues`](textcat_docs_issues) | Predicting whether a GitHub issue is about documentation (Text Classification) |
 | [`textcat_goemotions`](textcat_goemotions) | Categorization of emotions in Reddit posts (Text Classification) |
---
+++ tutorials/parser_low_resource/scripts/kfold.py
@@ -0,0 +1,126 @@
+import random
+import tempfile
+from pathlib import Path
+from typing import List, Optional
+
+import spacy
+import srsly
+import typer
+from spacy.cli._util import parse_config_overrides, setup_gpu
+from spacy.cli._util import show_validation_error
+from spacy.tokens import DocBin
+from spacy.training.corpus import Corpus
+from spacy.training.initialize import init_nlp
+from spacy.training.loop import train as train_nlp
+from spacy.util import load_config
+from wasabi import msg
+
+METRICS = ["token_acc", "pos_acc", "morph_acc", "tag_acc", "dep_uas", "dep_las"]
+
+
+def chunk(l: List, n: int):
+    """Split a list l into n chunks of fairly equal number of elements"""
+    k, m = divmod(len(l), n)
+    return (l[i * k + min(i, m) : (i + 1) * k + min(i + 1, m)] for i in range(n))
+
+
+def get_all_except(l: List, idx: int):
+    """Get all elements of a list except a given index"""
+    return l[:idx] + l[(idx + 1) :]
+
+
+def flatten(l: List) -> List:
+    """Flatten a list of lists"""
+    return [item for sublist in l for item in sublist]
+
+
+app = typer.Typer()
+
+
+@app.command(
+    context_settings={"allow_extra_args": True, "ignore_unknown_options": True}
+)
+def main(
+    # fmt: off
+    ctx: typer.Context,  # this is only used to read additional arguments
+    corpus_path: Path = typer.Argument(..., help="Path to the full corpus."),
+    config_path: Path = typer.Argument(..., help="Path to the spaCy configuration file."),
+    output_path: Path = typer.Option(..., "--output-path", "--output", "-o", help="Path to save the output scores (JSON)."),
+    n_folds: int = typer.Option(10, "--n-folds", "-n", help="Number of folds for cross-validation.", show_default=True),
+    lang: Optional[str] = typer.Option("tl", "--lang", "-l", help="Language vocab to use.", show_default=True),
+    shuffle: bool = typer.Option(False, "--shuffle", "-f", help="Flag for shuffling data"),
+    use_gpu: int = typer.Option(0, help="GPU id to use. Pass -1 to use the CPU."),
+    # fmt: on
+):
+    """Train a dependency parser with k-fold cross validation
+
+    This command-line interface allows training a spaCy pipeline using k-fold
+    cross validation. You can set the number of folds by passing a parameter to
+    '--n-folds'. It performs the split automatically, so you need to pass the
+    full corpus (not split into training/dev) in 'corpus_path'. Lastly,
+    we get the average of the scores for each fold to obtain the final metrics.
+    """
+    if n_folds <= 1:
+        raise ValueError("Cannot have folds less than or equal to 1.")
+
+    overrides = parse_config_overrides(ctx.args)
+    setup_gpu(use_gpu)
+
+    nlp = spacy.blank(lang)
+    doc_bin = DocBin().from_disk(corpus_path)
+    docs = list(doc_bin.get_docs(nlp.vocab))
+
+    if shuffle:
+        random.shuffle(docs)
+
+    folds = list(chunk(docs, n_folds))
+    all_scores = {metric: [] for metric in METRICS}
+    for idx, fold in enumerate(folds):
+        dev = fold
+        train = flatten(get_all_except(folds, idx=idx))
+        msg.divider(f"Fold {idx+1}, train: {len(train)}, dev: {len(dev)}")
+
+        # Save the train and test dataset into a temporary directory
+        # then train within the context of that directory
+        with tempfile.TemporaryDirectory() as tmpdir:
+
+            msg.info("Preparing data for training")
+            overrides["paths.train"] = str(Path(tmpdir) / "tmp_train.spacy")
+            overrides["paths.dev"] = str(Path(tmpdir) / "tmp_dev.spacy")
+            tmp_train_docbin = DocBin(docs=train)
+            tmp_train_docbin.to_disk(overrides["paths.train"])
+            tmp_dev_docbin = DocBin(docs=dev)
+            tmp_dev_docbin.to_disk(overrides["paths.dev"])
+            msg.good(
+                f"Temp files at {overrides['paths.train']} and {overrides['paths.dev']}"
+            )
+
+            msg.info("Training model for the current fold")
+            with show_validation_error(config_path, hint_fill=False):
+                config = load_config(config_path, overrides, interpolate=False)
+                nlp = init_nlp(config)
+
+            nlp, _ = train_nlp(nlp, None)
+
+            msg.info("Evaluating on the dev dataset")
+            corpus = Corpus(overrides["paths.dev"], gold_preproc=False)
+            dev_dataset = list(corpus(nlp))
+            scores = nlp.evaluate(dev_dataset)
+
+            # For our purposes, we'll only get the scores for the morphologizer,
+            # dependency parser, and POS tagger
+            for metric in METRICS:
+                all_scores[metric].append(scores[metric])
+
+    msg.info(f"Computing final {n_folds}-fold cross-validation score")
+    avg_scores = {
+        metric: sum(scores) / len(scores) for metric, scores in all_scores.items()
+    }
+    msg.table(avg_scores, header=("Metric", "Score"))
+    if output_path is not None:
+        srsly.write_json(output_path, avg_scores)
+        msg.good(f"Saved results to {output_path}")
+
+
+if __name__ == "__main__":
+    app()
