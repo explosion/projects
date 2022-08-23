d1460c752ffa7e1867cc78b7ccb69974e931d642
Paul O'Leary McCann <polm@dampfkraft.com>
2022-08-23 12:59:57 +0900

    Disable global env vars in test
    This re-enables the global env vars, and disables them in the coref
    test.
    
    The env vars override spacy configs to keep training short, but they are
    training-specific variables. Trying to use them in spacy assemble causes
    an error.

--- experimental/coref/test_project_coref.py
+++ experimental/coref/test_project_coref.py
@@ -1,9 +1,15 @@
+import os
 from spacy.cli.project.run import project_run
 from spacy.cli.project.assets import project_assets
 from pathlib import Path
 
 
 def test_experimental_coref():
+
+    # This is set globally in testing to make sure tests are fast, but can't be
+    # used for this project because passing training-specific parameters breaks
+    # the spacy assemble command.
+    os.environ["SPACY_CONFIG_OVERRIDES"] = ""
     root = Path(__file__).parent
     # provide a placeholder value here, since we won't have real OntoNotes
     overrides = {
