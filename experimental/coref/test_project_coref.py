a3eef6dec640e40c397c48cc3e3c915c860ae04f
Paul O'Leary McCann <polm@dampfkraft.com>
2022-09-14 12:47:23 +0900

    Save and restore the spacy overrides env var

--- experimental/coref/test_project_coref.py
+++ experimental/coref/test_project_coref.py
@@ -8,8 +8,11 @@ def test_experimental_coref():
 
     # This is set globally in testing to make sure tests are fast, but can't be
     # used for this project because passing training-specific parameters breaks
-    # the spacy assemble command.
+    # the spacy assemble command. We need to restore the old value after this
+    # test though.
+    old_overrides = os.environ.get("SPACY_CONFIG_OVERRIDES", "")
     os.environ["SPACY_CONFIG_OVERRIDES"] = ""
+
     root = Path(__file__).parent
     # provide a placeholder value here, since we won't have real OntoNotes
     overrides = {
@@ -18,5 +21,10 @@ def test_experimental_coref():
         "vars.max_epochs": 10,
         "vars.config_dir": "configs/test",
     }
-    project_assets(root, overrides=overrides, extra=True)
-    project_run(root, "ci-test", overrides=overrides)
+    try:
+        project_assets(root, overrides=overrides, extra=True)
+        project_run(root, "ci-test", overrides=overrides)
+    except:
+        raise
+    finally:
+        os.environ["SPACY_CONFIG_OVERRIDES"] = old_overrides
