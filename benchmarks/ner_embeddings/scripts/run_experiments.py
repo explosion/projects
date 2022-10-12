60964660ff6cf06f87aa5c401ac3a12b34a09e5a
Lj Miranda <ljvmiranda@gmail.com>
2022-10-13 06:43:21 +0800

    Run all datasets now

--- benchmarks/ner_embeddings/scripts/run_experiments.py
+++ benchmarks/ner_embeddings/scripts/run_experiments.py
@@ -15,10 +15,10 @@ app = typer.Typer()
 
 # A mapping of datasets and their vectors
 DATASET_VECTORS = {
-    # "archaeo": {"spacy": "nl_core_news_lg", "fasttext": "fasttext-nl", "lang": "nl"},
-    # "anem": {"spacy": "en_core_web_lg", "fasttext": "fasttext-en", "lang": "en"},
-    # "es-conll": {"spacy": "es_core_news_lg", "fasttext": "fasttext-es", "lang": "es"},
-    # "nl-conll": {"spacy": "nl_core_news_lg", "fasttext": "fasttext-nl", "lang": "nl"},
+    "archaeo": {"spacy": "nl_core_news_lg", "fasttext": "fasttext-nl", "lang": "nl"},
+    "anem": {"spacy": "en_core_web_lg", "fasttext": "fasttext-en", "lang": "en"},
+    "es-conll": {"spacy": "es_core_news_lg", "fasttext": "fasttext-es", "lang": "es"},
+    "nl-conll": {"spacy": "nl_core_news_lg", "fasttext": "fasttext-nl", "lang": "nl"},
     "wnut17": {"spacy": "en_core_web_lg", "fasttext": "fasttext-en", "lang": "en"},
 }
 
