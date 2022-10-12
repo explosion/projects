fb3e52d20bc8f150fbd8fadb72b236c89ca82a84
Lj Miranda <ljvmiranda@gmail.com>
2022-10-12 13:06:06 +0800

    Add WNUT dataset

--- benchmarks/ner_embeddings/scripts/run_experiments.py
+++ benchmarks/ner_embeddings/scripts/run_experiments.py
@@ -19,6 +19,7 @@ DATASET_VECTORS = {
     "anem": {"spacy": "en_core_web_lg", "fasttext": "fasttext-en", "lang": "en"},
     "es-conll": {"spacy": "es_core_news_lg", "fasttext": "fasttext-es", "lang": "es"},
     "nl-conll": {"spacy": "nl_core_news_lg", "fasttext": "fasttext-nl", "lang": "nl"},
+    "wnut17": {"spacy": "en_core_web_lg", "fasttext": "fasttext-en", "lang": "en"},
 }
 
 
