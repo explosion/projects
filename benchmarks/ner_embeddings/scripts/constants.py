5b2214a3d41020657c36d94a92046fec3543da11
Lj Miranda <ljvmiranda@gmail.com>
2022-10-14 13:04:28 +0800

    Use a constants util for reusable params

---
+++ benchmarks/ner_embeddings/scripts/constants.py
@@ -0,0 +1,10 @@
+# A mapping of datasets and their vectors
+DATASET_VECTORS = {
+    "archaeo": {"spacy": "nl_core_news_lg", "fasttext": "fasttext-nl", "lang": "nl"},
+    "anem": {"spacy": "en_core_web_lg", "fasttext": "fasttext-en", "lang": "en"},
+    "es-conll": {"spacy": "es_core_news_lg", "fasttext": "fasttext-es", "lang": "es"},
+    "nl-conll": {"spacy": "nl_core_news_lg", "fasttext": "fasttext-nl", "lang": "nl"},
+    "wnut17": {"spacy": "en_core_web_lg", "fasttext": "fasttext-en", "lang": "en"},
+}
+
+CONFIGS = ["ner_multiembed", "ner_multihashembed"]
--- benchmarks/ner_embeddings/scripts/run_experiments.py
+++ benchmarks/ner_embeddings/scripts/run_experiments.py
@@ -8,19 +8,12 @@ import srsly
 import typer
 from wasabi import msg
 
+from .constants import DATASET_VECTORS
+
 Arg = typer.Argument
 Opt = typer.Option
 
 app = typer.Typer()
-
-# A mapping of datasets and their vectors
-DATASET_VECTORS = {
-    "archaeo": {"spacy": "nl_core_news_lg", "fasttext": "fasttext-nl", "lang": "nl"},
-    "anem": {"spacy": "en_core_web_lg", "fasttext": "fasttext-en", "lang": "en"},
-    "es-conll": {"spacy": "es_core_news_lg", "fasttext": "fasttext-es", "lang": "es"},
-    "nl-conll": {"spacy": "nl_core_news_lg", "fasttext": "fasttext-nl", "lang": "nl"},
-    "wnut17": {"spacy": "en_core_web_lg", "fasttext": "fasttext-en", "lang": "en"},
-}
 
 
 class StaticVectors(str, Enum):
