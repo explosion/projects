32ab441a079242cffc133d3bae3ee9efc217437b
Raphael Mitsch <r.mitsch@outlook.com>
2022-10-27 10:38:01 +0200

    Disable overrides temporarily for wikid calls.

--- benchmarks/nel/test_nel_benchmark.py
+++ benchmarks/nel/test_nel_benchmark.py
@@ -1,4 +1,6 @@
 """ Testing all project steps. """
+import os
+
 import pytest
 from pathlib import Path
 import sys
@@ -12,11 +14,15 @@ def test_nel_benchmark():
     project_run(root, "download_model", capture=True)
     project_run(root, "wikid_clone", capture=True)
     project_run(root, "preprocess", capture=True)
+    overrides_key = "SPACY_CONFIG_OVERRIDES"
+    overrides = os.environ.pop(overrides_key) if overrides_key in os.environ else None
     project_run(root, "wikid_download_assets", capture=True)
     project_run(root, "wikid_parse", capture=True)
     project_run(root, "wikid_create_kb", capture=True)
+    if overrides:
+        os.environ[overrides_key] = overrides
     project_run(root, "parse_corpus", capture=True)
     project_run(root, "compile_corpora", capture=True)
-    project_run(root, "train", capture=True, overrides={"vars.training_max_steps": 1})
+    project_run(root, "train", capture=True)
     project_run(root, "evaluate", capture=True)
     project_run(root, "compare_evaluations", capture=True)
