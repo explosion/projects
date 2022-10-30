17feeebe351776ccf3e102bd5b54a76e84b732f0
morgoth95 <d.fiori@nebuly.ai>
2022-10-30 11:11:51 +0100

    edit test

--- experimental/ner_wikiner_accelerated_with_nebullvm/test_project_ner_wikiner.py
+++ experimental/ner_wikiner_accelerated_with_nebullvm/test_project_ner_wikiner.py
@@ -1,10 +1,8 @@
-import pytest
 from spacy.cli.project.run import project_run
 from spacy.cli.project.assets import project_assets
 from pathlib import Path
 
 
-@pytest.mark.skip(reason="TODO: numpy error on Windows")
 def test_wikiner_project():
     root = Path(__file__).parent
     project_assets(root)
