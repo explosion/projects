0f673723e5ef243f2116a2db73f5b209e1228cc0
morgoth95 <d.fiori@nebuly.ai>
2022-10-31 10:15:06 +0100

    skip test on windows

--- experimental/ner_wikiner_accelerated_with_nebullvm/test_project_ner_wikiner.py
+++ experimental/ner_wikiner_accelerated_with_nebullvm/test_project_ner_wikiner.py
@@ -1,9 +1,15 @@
+import sys
+from pathlib import Path
+
 from spacy.cli.project.run import project_run
 from spacy.cli.project.assets import project_assets
-from pathlib import Path
 
 
 def test_wikiner_project():
+    if sys.platform.startswith('win'):
+        # skip test on windows since nebullvm is not supporting windows
+        #  devices yet. 
+        return
     root = Path(__file__).parent
     project_assets(root)
     project_run(root, "all", capture=True)
