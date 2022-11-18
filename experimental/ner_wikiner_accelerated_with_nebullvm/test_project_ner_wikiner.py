f9a36850c8107715eed8efa0287683b73e938131
Adriane Boyd <adrianeboyd@gmail.com>
2022-11-18 16:20:56 +0100

    Adjust project settings to reduce time for tests, reduce telemetry

--- experimental/ner_wikiner_accelerated_with_nebullvm/test_project_ner_wikiner.py
+++ experimental/ner_wikiner_accelerated_with_nebullvm/test_project_ner_wikiner.py
@@ -8,6 +8,13 @@ from spacy.cli.project.assets import project_assets
 
 @pytest.mark.skipif(sys.platform == "win32", reason="nebullvm does support windows devices yet")
 def test_wikiner_project():
+    import openvino_telemetry as tm
+    tm.Telemetry.opt_out("UA-17808594-29")
+    overrides = {
+        "vars.optimize_opts": "-ot constrained",
+        "vars.corpora_dev_limit": 10,
+        "vars.corpora_train_limit": 10,
+    }
     root = Path(__file__).parent
     project_assets(root)
-    project_run(root, "all", capture=True)
+    project_run(root, "all", overrides=overrides, capture=True)
