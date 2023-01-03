562507326dc3e43238b49c3943f97285541ad25d
svlandeg <svlandeg@github.com>
2023-01-03 19:07:49 +0100

    give test a unique name

--- experimental/ner_wikiner_speedster/test_project_ner_wikiner_speedster.py
+++ experimental/ner_wikiner_speedster/test_project_ner_wikiner_speedster.py
@@ -4,7 +4,7 @@ from spacy.cli.project.run import project_run
 from spacy.cli.project.assets import project_assets
 
 
-def test_wikiner_project():
+def test_wikiner_speedster_project():
     import openvino_telemetry as tm
     tm.Telemetry.opt_out("UA-17808594-29")
     overrides = {
