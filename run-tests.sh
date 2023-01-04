66de007f62555ce2d8210c5b7fed813576fa8674
Paul O'Leary McCann <polm@dampfkraft.com>
2023-01-04 18:06:08 +0900

    Simplify test running script to work with bash 3
    This only uses old bash features, avoiding arrays.

--- run-tests.sh
+++ run-tests.sh
@@ -3,30 +3,20 @@
 # Only run tests in second-level directories that have been changed in the last commit.
 
 # Fetch changed files.
-mapfile -t dirs < <( git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g')
-declare -a tested_dirs=()
+function get_dirs() {
+  git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g' | cut -f 1-2 -d/ | grep / | sort -u
+}
+
 exit_code=0
-for dir in "${dirs[@]}"
+get_dirs | while read dir
 do
-  # Get path with second level only. This will be empty if the change happened at the first level.
-  second_level_dir=$(echo "$dir" | awk -F/ '{print FS $2}')
-  second_level_dir="${second_level_dir:1}"
-  # Get path with first level/second level.
-  full_second_level_dir=$(echo "$dir" | cut -d/ -f1-2)
-
-  # Only run if change happened at second level, since first level-changes don't require the tests to be re-run.
-  if [ ! -z "$second_level_dir" -a "$second_level_dir" != " " ]; then
-    if [[ ! " ${tested_dirs[*]} " =~ " ${full_second_level_dir} " ]]; then
-      tested_dirs+=($full_second_level_dir)
-      if [ -e $full_second_level_dir/requirements.txt ]; then
-        python -m pip install -q -r $full_second_level_dir/requirements.txt --extra-index-url https://download.pytorch.org/whl/cpu
-      fi
-      python -m pytest -s $full_second_level_dir
-      if [[ $? != @(0|5) ]]; then
-        exit_code=1
-      fi
-    fi
+  if [ -e $dir/requirements.txt ]; then
+    python -m pip install -q -r $dir/requirements.txt --extra-index-url https://download.pytorch.org/whl/cpu
   fi
-
+  python -m pytest -s $dir
+  # exit code 5 means no tests were found
+  if [[ $? != 0 || $? != 5 ]]; then
+    exit_code=1
+  fi
 done
 exit $exit_code
