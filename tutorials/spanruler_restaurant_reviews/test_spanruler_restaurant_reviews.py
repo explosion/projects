d42896cb5c715c911d4664a5c143b78cc575bc6d
Lj Miranda <ljvmiranda@gmail.com>
2023-01-06 11:44:12 +0800

    Format code with black

--- tutorials/spanruler_restaurant_reviews/test_spanruler_restaurant_reviews.py
+++ tutorials/spanruler_restaurant_reviews/test_spanruler_restaurant_reviews.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from spacy.cli.project.run import project_run
 from spacy.cli.project.assets import project_assets
 
+
 def test_spanruler_restaurant_reviews():
     root = Path(__file__).parent
     project_assets(root)
