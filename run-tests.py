7cf21d3f798d251f9e515eaf385274be32777224
Raphael Mitsch <r.mitsch@outlook.com>
2023-01-13 12:13:45 +0100

    Improve logging. Remove redundant comments.

--- run-tests.py
+++ run-tests.py
@@ -50,7 +50,8 @@ def _log(logger: logging.Logger, verbosity: Optional[str], required_verbosity: s
     """
     Logs text, if the verbosity level is sufficient.
     verbosity (Optional[str]): Verbosity level. If None, nothing is logged. If "v", step description is logged. If "vv",
-        step description and pytest output are logged.
+        step description, pytest output and, before terminating the script, a complete list of stdout/stderr/return
+        codes script) are logged.
     required_verbosity (Optional[str]): Required verbosity level to log this text.
     """
     if verbosity == required_verbosity or verbosity == required_verbosity + "v":
@@ -67,14 +68,14 @@ def main(
     python (Path): Path to Python interpreter to use for tests.
     run_all (bool): Whether to run tests for all projects. If False, only tests for changed directories are run.
     verbosity (Optional[str]): Verbosity level. If None, nothing is logged. If "v", step description is logged. If "vv",
-        step description and pytest output are logged.
+        pytest output and detailed information are logged.
     RETURN (int): Return code.
     """
     logger = _get_logger()
     cmds_status: List[Dict[str, Union[str, int, List[str]]]] = []
-    top_level_dirs = ("benchmarks", ) # "benchmarks", "experimental", "integrations", "pipelines", "tutorials"
+    top_level_dirs = ("benchmarks", "experimental", "integrations", "pipelines", "tutorials")
 
-    _log(logger, verbosity, "v", "Fetching projects to test")
+    _log(logger, verbosity, "v", "Scanning projects")
     if run_all:
         proj_dirs = [
             path for path in
@@ -92,34 +93,32 @@ def main(
             if Path(file).parent != Path(".")
         }
 
-    # todo run tests.
     for proj_dir in proj_dirs:
         _log(logger, verbosity, "v", f"*** {proj_dir} ***")
+
         # Install from requirements.txt, if it exists.
         if (proj_dir / "requirements.txt").exists():
-            _log(logger, verbosity, "v", "Installing requirements")
+            _log(logger, verbosity, "v", "  - Installing requirements")
             _run([python, "-m", "pip", "-q", "install", "-r", proj_dir / "requirements.txt"], cmds_status)
 
         # Fetch spacy version from project.yml, install spacy version if available.
         spacy_version = srsly.read_yaml(proj_dir / "project.yml").get("spacy_version")
         if spacy_version:
-            _log(logger, verbosity, "v", f"  Installing spacy{spacy_version} from project.yml")
+            _log(logger, verbosity, "v", f"  - Installing spacy{spacy_version} from project.yml")
             _run([python, "-m", "pip", "-q", "install", f"spacy{spacy_version}", "--force-reinstall"], cmds_status)
 
-        _log(logger, verbosity, "v", "  Running tests")
+        _log(logger, verbosity, "v", "  - Running tests")
         _run([python, "-m", "pytest", "-q", "-s", proj_dir], cmds_status)
+        # Log stderr and stdout.
         if cmds_status[-1]["returncode"] not in (0, 5):
-            _log(logger, verbosity, "vv", "    ### stderr ###")
-            for out in cmds_status[-1]["stderr"]:
-                _log(logger, verbosity, "vv", f"    {out}")
-            _log(logger, verbosity, "vv", "    ### stdout ###")
-            # Sometimes errors are printed to stdout.
-            for out in cmds_status[-1]["stdout"]:
-                _log(logger, verbosity, "vv", f"    {out}")
+            for channel in ("stderr", "stdout"):
+                _log(logger, verbosity, "vv", f"    ### {channel} ###")
+                for out in cmds_status[-1][channel]:
+                    _log(logger, verbosity, "vv", f"    {out}")
         elif cmds_status[-1]["returncode"] == 5:
             _log(logger, verbosity, "vv", "    No tests found")
 
-        _log(logger, verbosity, "vv", "  Restoring environment")
+        _log(logger, verbosity, "v", "  - Restoring environment")
         with tempfile.NamedTemporaryFile("w") as file:
             file.writelines(
                 _run(
@@ -132,11 +131,15 @@ def main(
     for cmd_status in cmds_status:
         _log(logger, verbosity, "vv", str(cmd_status))
 
-    # Return 0 if all commands hat return code 0, 1 otherwise.
-    # todo doesnt seem to work?
-    return not all([cmd_status["returncode"] == 0 for cmd_status in cmds_status])
+    # failing:
+    #   - experimental/ner_wikiner_speedster (GIL error, maybe due to python 3.9?)
+
+    # Return 0/True if all commands have return code 0 or 5 (no tests found), 1/False otherwise.
+    return int(any([cmd_status["returncode"] not in (0, 5) for cmd_status in cmds_status]))
 
 
 if __name__ == '__main__':
-    main(Path(".venv") / "bin" / "python", True, "vv")
+    # rm -rf .venv && python -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt && source .env/bin/activate
+    if main(Path(".venv") / "bin" / "python", True, "vv") != 0:
+        raise Exception("Test run failed. Review logs for more details.")
     # typer.run(main)
