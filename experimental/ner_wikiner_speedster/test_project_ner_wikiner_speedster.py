6b33aa84e4770104971eeaffb3f9f0c3564c0854
Adriane Boyd <adrianeboyd@gmail.com>
2023-01-27 12:25:27 +0100

    Remove capture=True

--- experimental/ner_wikiner_speedster/test_project_ner_wikiner_speedster.py
+++ experimental/ner_wikiner_speedster/test_project_ner_wikiner_speedster.py
@@ -13,4 +13,4 @@ def test_wikiner_speedster_project():
     }
     root = Path(__file__).parent
     project_assets(root)
-    project_run(root, "all", overrides=overrides, capture=True, encoding=sys.getdefaultencoding())
+    project_run(root, "all", overrides=overrides)
