ee6cd2a363a09f2c21f7cae2b56c5a79bd6e30a6
Adriane Boyd <adrianeboyd@gmail.com>
2023-07-31 11:34:38 +0200

    Add pretraining workflow to tests

--- benchmarks/pretraining_morphologizer_oscar/test_project_pretraining.py
+++ benchmarks/pretraining_morphologizer_oscar/test_project_pretraining.py
@@ -7,3 +7,4 @@ def test_project_pretraining():
     root = Path(__file__).parent
     project_assets(root)
     project_run(root, "data")
+    project_run(root, "training_char", overrides={"vars.epochs": 1})
